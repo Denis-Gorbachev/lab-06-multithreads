871e1bde7ada034645a6468420341de27bf83d66
Denis Gorbachev <denis.gorbachev2002@yandex.ru>
2022-03-27 13:48:08 +0300

    First commit

--- include/thread.hpp
+++ include/thread.hpp
@@ -1,8 +1,6 @@
 // Copyright 2021 Your Name <your_email>
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_THREAD_HPP_
+#define INCLUDE_THREAD_HPP_
 
-auto example() -> void;
-
-#endif // INCLUDE_EXAMPLE_HPP_
+#endif // INCLUDE_THREAD_HPP_
